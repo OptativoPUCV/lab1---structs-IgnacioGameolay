2f71a63257f22481dd1d0ac5cacb45fb6c5649c5
Your Name <you@example.com>
2024-03-15 21:48:05 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -68,7 +68,7 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
   ///*result = (int *)malloc(sizeof(int) * (size3));
   int i = 0, j = 0, k = 0;
 
-  /*
+  
   while (i < size1 && j < size2) {
     if (arr1[i] < arr2[j]) {
       result[k] = arr1[i];
@@ -80,17 +80,7 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
     }
     k++;
   }
-  */
-  for ( ; i < size1 && j < size2; k++){
-    if (arr1[i] < arr2[j]) {
-      result[k] = arr1[i];
-      i++;
-
-    } else {
-      result[k] = arr2[j];
-      j++;
-    }
-  }
+  
 }
 
 /*
